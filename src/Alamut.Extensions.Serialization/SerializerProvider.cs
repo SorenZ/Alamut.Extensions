aa72d0ca5d068382dd97bab2f99d24375b3f6218
Hamed ZVand <sunrise.freedom@gmail.com>
2019-09-24 11:53:13 +0330

    added SetDefaultSerializer in SerializerProvider

--- src/Alamut.Extensions.Serialization/SerializerProvider.cs
+++ src/Alamut.Extensions.Serialization/SerializerProvider.cs
@@ -1,4 +1,5 @@
-﻿using Alamut.Extensions.Serialization.Providers;
+﻿using System;
+using Alamut.Extensions.Serialization.Providers;
 
 namespace Alamut.Extensions.Serialization
 {
@@ -6,10 +7,34 @@ namespace Alamut.Extensions.Serialization
     {
         static SerializerProvider()
         {
-            //Default = new MessagePackSerializer();
-            Default = new NewtonsoftJsonSerializer();
+            SetDefaultSerializer(SerializerProviders.MessagePack);
         }
 
-        public static ISerializer Default { get; }
+        public static ISerializer Default { get; private set; }
+
+        public static void SetDefaultSerializer(SerializerProviders provider)
+        {
+            switch (provider)
+            {
+                case SerializerProviders.MessagePack:
+                    Default = new MessagePackSerializer();
+                    break;
+
+                case SerializerProviders.NewtonsoftJson:
+                    Default = new NewtonsoftJsonSerializer();
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(provider), provider, null);
+            }
+        }
+
+        public static void SetDefaultSerializer(ISerializer customSerializer) => Default = customSerializer;
+    }
+
+    public enum SerializerProviders
+    {
+        MessagePack = 0,
+        NewtonsoftJson = 1
     }
 }
